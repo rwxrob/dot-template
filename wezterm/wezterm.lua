b3ab463f68bd61c6336320bf2ec581fa0bad7438
Rob Muhlestein <rob@rwx.gg>
2024-12-02 16:55:03 -0500

    update wezterm

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -2,25 +2,26 @@ local wezterm = require("wezterm")
 
 return {
 
-  font_size = 36,
+  font_size = 41, --so people helping you can see the screen
   font = wezterm.font("UbuntuMono Nerd Font"),
+  color_scheme = 'Gruvbox Material (Gogh)',
 
-  initial_cols = 83,
-  initial_rows = 25,
+  --window_decorations = "RESIZE",
+  --enable_tab_bar = false,
+  --hide_tab_bar_if_only_one_tab = true,
+
+  initial_cols = 74, --deliberately small to promote good coding
+  initial_rows = 22,
 
   window_padding = {
-   left = 2,
+   left = 40,
    right = 0,
    top = 0,
-   bottom = 0,
+   bottom = 2,
   },
 
   term = "xterm-254color",
   animation_fps = 60,
-  max_fps = 60,
-  window_decorations = "RESIZE",
+  max_fps = 60
 
-  enable_tab_bar = false,
-  hide_tab_bar_if_only_one_tab = true,
-  color_scheme = 'Gruvbox Material (Gogh)'
 }
