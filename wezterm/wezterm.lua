afcd7b87980f488e0a89563ad2d2575002b0a4dd
Rob Muhlestein <rob@rwx.gg>
2024-12-02 17:28:05 -0500

    add wezterm to setup script

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,17 +1,19 @@
 local wezterm = require("wezterm")
+local mux = wezterm.mux
 
 return {
+  window_close_confirmation = 'NeverPrompt',
 
-  font_size = 41, --so people helping you can see the screen
+  font_size = 41,
   font = wezterm.font("UbuntuMono Nerd Font"),
-  color_scheme = 'Gruvbox Material (Gogh)',
 
-  --window_decorations = "RESIZE",
-  --enable_tab_bar = false,
-  --hide_tab_bar_if_only_one_tab = true,
-
-  initial_cols = 74, --deliberately small to promote good coding
+  initial_cols = 74,
   initial_rows = 22,
+
+  wezterm.on('gui-startup', function(cmd)
+    local tab, pane, window = mux.spawn_window(cmd or {})
+    window:gui_window():set_position(0, 130)
+end),
 
   window_padding = {
    left = 40,
@@ -22,6 +24,10 @@ return {
 
   term = "xterm-254color",
   animation_fps = 60,
-  max_fps = 60
+  max_fps = 60,
+  window_decorations = "RESIZE",
 
+  enable_tab_bar = false,
+  hide_tab_bar_if_only_one_tab = true,
+  color_scheme = 'Gruvbox Material (Gogh)'
 }
