7cd62e716f18a086a1be9ef9188ce7f573f235cb
Rob Muhlestein <rob@rwx.gg>
2024-12-02 15:46:42 -0500

    use built-in theme for gruvbox-material

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -5,16 +5,11 @@ return {
   font_size = 36,
   font = wezterm.font("UbuntuMono Nerd Font"),
 
-  keys = {
-    -- Remove any existing binding for Option + -
-    { key = "_", mods = "OPT|SHIFT", action = "DisableDefaultAssignment" },
-  },
-
   initial_cols = 83,
   initial_rows = 25,
 
   window_padding = {
-   left = 0,
+   left = 2,
    right = 0,
    top = 0,
    bottom = 0,
@@ -27,36 +22,5 @@ return {
 
   enable_tab_bar = false,
   hide_tab_bar_if_only_one_tab = true,
-
-  --window_background_opacity = 0.8,
-
-  colors = {
-     foreground = "#d4be98", -- Default text color
-     cursor_bg  = "#d4be98", -- Default text color
-     cursor_border = "#d4be98", -- Default text color
-     background = "#282828", -- Background color
-     selection_bg = "#45403d" ,
-
-    ansi = {
-      "#282828", -- Black (Normal)
-      "#cc241d", -- Red (Normal)
-      "#98971a", -- Green (Normal)
-      "#d79921", -- Yellow (Normal)
-      "#458588", -- Blue (Normal)
-      "#b16286", -- Magenta (Normal)
-      "#689d6a", -- Cyan (Normal)
-      "#a89984", -- White (Normal)
-    },
-    brights = {
-      "#928374", -- Bright Black
-      "#fb4934", -- Bright Red
-      "#b8bb26", -- Bright Green
-      "#fabd2f", -- Bright Yellow
-      "#83a598", -- Bright Blue
-      "#d3869b", -- Bright Magenta
-      "#8ec07c", -- Bright Cyan
-      "#ebdbb2", -- Bright White
-    }
-  }
-
+  color_scheme = 'Gruvbox Material (Gogh)'
 }
