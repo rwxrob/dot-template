d5aa65ac8c4961e5fea7f703716dd1711057b963
Rob Muhlestein <rob@rwx.gg>
2024-12-02 19:39:18 -0500

    add fallback fonts

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,11 +1,30 @@
 local wezterm = require("wezterm")
 local mux = wezterm.mux
 
+local function detect_shell()
+  if wezterm.target_triple == "x86_64-pc-windows-msvc" then
+    -- Path to Git Bash on Windows
+    return {
+      "C:\\Program Files\\Git\\bin\\bash.exe",
+      "--login",
+      "-i",
+    }
+  else
+    -- Default to bash for all other systems
+    return {"/bin/bash", "--login"}
+  end
+end
+
 return {
   window_close_confirmation = 'NeverPrompt',
+  default_prog = detect_shell(),
 
   font_size = 41,
-  font = wezterm.font("UbuntuMono Nerd Font"),
+  font = wezterm.font_with_fallback(
+    "UbuntuMono Nerd Font",                  -- installed
+    "Apple Color Emoji", "SF Mono", "Menlo", -- mac builtins
+    "Segoe UI Emoji", "Consolas"             -- win builtins
+  ),
 
   initial_cols = 74,
   initial_rows = 22,
@@ -31,3 +50,8 @@ end),
   hide_tab_bar_if_only_one_tab = true,
   color_scheme = 'Gruvbox Material (Gogh)'
 }
+
+
+
+
+
