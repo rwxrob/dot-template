312ab622ed788ecfccaa1d44a553ad883185a87b
Rob Muhlestein <rob@rwx.gg>
2024-12-03 12:13:05 -0500

    several vim and wezterm fixes

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,34 +1,59 @@
 local wezterm = require("wezterm")
 local mux = wezterm.mux
 
+----------------------------- detect_os ----------------------------
+
+local function detect_os()
+  if wezterm.target_triple == "x86_64-apple-darwin" or wezterm.target_triple == "aarch64-apple-darwin" then
+    return "macos"
+  elseif wezterm.target_triple == "x86_64-pc-windows-msvc" then
+    return "windows"
+  elseif wezterm.target_triple == "x86_64-unknown-linux-gnu" then
+    return "linux"
+  else
+    return "unknown"
+  end
+end
+
+local os = detect_os()
+
+--------------------------- detect_shell ---------------------------
+
 local function detect_shell()
-  if wezterm.target_triple == "x86_64-pc-windows-msvc" then
-    -- Path to Git Bash on Windows
+  if os == "windows" then
     return {
       "C:\\Program Files\\Git\\bin\\bash.exe",
       "--login",
       "-i",
     }
+  elseif os == "mac" then
+    return { "/opt/homebrew/bin/bash", "--login", }
   else
-    -- Default to bash for all other systems
     return {"/bin/bash", "--login"}
   end
 end
+
+---------------------------- detect_font ---------------------------
+
+local function detect_font()
+  if os == "windows" then
+    return { "Ubuntu Mono" }
+  elseif os == "mac" then
+    return { "UbuntuMono Nerd Font" }
+  else
+    return font
+  end
+end
+
+------------------------------- main -------------------------------
 
 return {
   window_close_confirmation = 'NeverPrompt',
   default_prog = detect_shell(),
 
   color_scheme = 'Gruvbox Material (Gogh)',
-  font = wezterm.font("Ubuntu Mono"),
+  font = detect_font(),
   font_size = 26,
-  --initial_cols = 74,
-  --initial_rows = 22,
-
-  wezterm.on('gui-startup', function(cmd)
-    local tab, pane, window = mux.spawn_window(cmd or {})
-    window:gui_window():set_position(0, 130)
-end),
 
   window_padding = {
    left = 40,
@@ -37,13 +62,28 @@ end),
    bottom = 2,
   },
 
-  term = "xterm-254color",
+  term = "xterm-256color",
   animation_fps = 60,
-  max_fps = 60
+  max_fps = 60,
+
+  ----------- rwxrob streaming stuff, fyi --------------
+
+  --font_size = 41,
+  --initial_cols = 74,
+  --initial_rows = 22,
+
+  --[[
+  -- exactly position
+  wezterm.on('gui-startup', function(cmd)
+    local tab, pane, window = mux.spawn_window(cmd or {})
+    window:gui_window():set_position(0, 130)
+end),
+  ]]--
 
   --window_decorations = "RESIZE",
   --enable_tab_bar = false,
   --hide_tab_bar_if_only_one_tab = true,
+
 }
 
 
